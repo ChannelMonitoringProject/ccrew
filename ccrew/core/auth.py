a2bc433c8acd3d3c02135f349198f4a60509aec4
yussidivnall <UC.dev.null@gmail.com>
2025-05-11 19:22:47 +0200

    Changed to Flask-SecurityToo and updated role

--- ccrew/core/auth.py
+++ ccrew/core/auth.py
@@ -26,5 +26,9 @@ def seed_auth(security: Security):
             raise ValueError(
                 "Missing user/password to seed, set environment ADMIN_USER and ADMIN_PASSWORD first"
             )
-        security.datastore.create_role(name="admin")
-        security.datastore.create_user(email=user, password=hash_password(password))
+        admin_role = security.datastore.find_or_create_role(name="admin")
+        admin_user = security.datastore.create_user(
+            email=user, password=hash_password(password)
+        )
+        security.datastore.add_role_to_user(admin_user, admin_role)
+        security.datastore.commit()
